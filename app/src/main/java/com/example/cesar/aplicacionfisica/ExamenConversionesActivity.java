ce203318587924b402aee2db291dedd1f176cfd8
cesar <cesa.cotobvb@gmail.com>
2018-01-04 22:56:26 -0600

    Diseño responsivo de las actividades examen teorico vectores examen practico conversiones y examen practico vectores. actualizacion de diseño en las actividades vectores y conversiones

--- app/src/main/java/com/example/cesar/aplicacionfisica/ExamenConversionesActivity.java
+++ app/src/main/java/com/example/cesar/aplicacionfisica/ExamenConversionesActivity.java
@@ -1,13 +1,176 @@
 package com.example.cesar.aplicacionfisica;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class ExamenConversionesActivity extends AppCompatActivity {
+
+
+    //se crean las vistas y enlazan con ayuda de butterKnife
+    @BindView(R.id.respuestaUnoPreguntaUno)
+    RadioButton respuestaUnoPreguntaUno;
+    @BindView(R.id.respuestaDosPreguntaUno)
+    RadioButton respuestaDosPreguntaUno;
+    @BindView(R.id.respuestaTresPreguntaUno)
+    RadioButton respuestaTresPreguntaUno;
+    @BindView(R.id.respuestaCuatroPreguntaUno)
+    RadioButton respuestaCuatroPreguntaUno;
+    @BindView(R.id.grupoRespuestasUno)
+    RadioGroup grupoRespuestasUno;
+    @BindView(R.id.respuestaUnoPreguntaDos)
+    RadioButton respuestaUnoPreguntaDos;
+    @BindView(R.id.respuestaDosPreguntaDos)
+    RadioButton respuestaDosPreguntaDos;
+    @BindView(R.id.respuestaTresPreguntaDos)
+    RadioButton respuestaTresPreguntaDos;
+    @BindView(R.id.respuestaCuatroPreguntaDos)
+    RadioButton respuestaCuatroPreguntaDos;
+    @BindView(R.id.grupoRespuestasDos)
+    RadioGroup grupoRespuestasDos;
+    @BindView(R.id.respuestaUnoPreguntaTres)
+    RadioButton respuestaUnoPreguntaTres;
+    @BindView(R.id.respuestaDosPreguntaTres)
+    RadioButton respuestaDosPreguntaTres;
+    @BindView(R.id.respuestaTresPreguntaTres)
+    RadioButton respuestaTresPreguntaTres;
+    @BindView(R.id.respuestaCuatroPreguntaTres)
+    RadioButton respuestaCuatroPreguntaTres;
+    @BindView(R.id.grupoRespuestasTres)
+    RadioGroup grupoRespuestasTres;
+    @BindView(R.id.respuestaUnoPreguntaCuatro)
+    RadioButton respuestaUnoPreguntaCuatro;
+    @BindView(R.id.respuestaDosPreguntaCuatro)
+    RadioButton respuestaDosPreguntaCuatro;
+    @BindView(R.id.respuestaTresPreguntaCuatro)
+    RadioButton respuestaTresPreguntaCuatro;
+    @BindView(R.id.respuestaCuatroPreguntaCuatro)
+    RadioButton respuestaCuatroPreguntaCuatro;
+    @BindView(R.id.grupoRespuestasCuatro)
+    RadioGroup grupoRespuestasCuatro;
+    @BindView(R.id.respuestaUnoPreguntaCinco)
+    RadioButton respuestaUnoPreguntaCinco;
+    @BindView(R.id.respuestaDosPreguntaCinco)
+    RadioButton respuestaDosPreguntaCinco;
+    @BindView(R.id.respuestaTresPreguntaCinco)
+    RadioButton respuestaTresPreguntaCinco;
+    @BindView(R.id.respuestaCuatroPreguntaCinco)
+    RadioButton respuestaCuatroPreguntaCinco;
+    @BindView(R.id.grupoRespuestasCinco)
+    RadioGroup grupoRespuestasCinco;
+    @BindView(R.id.respuestaUnoPreguntaSeis)
+    RadioButton respuestaUnoPreguntaSeis;
+    @BindView(R.id.respuestaDosPreguntaSeis)
+    RadioButton respuestaDosPreguntaSeis;
+    @BindView(R.id.respuestaTresPreguntaSeis)
+    RadioButton respuestaTresPreguntaSeis;
+    @BindView(R.id.respuestaCuatroPreguntaSeis)
+    RadioButton respuestaCuatroPreguntaSeis;
+    @BindView(R.id.grupoRespuestasSeis)
+    RadioGroup grupoRespuestasSeis;
+    @BindView(R.id.respuestaUnoPreguntaSiete)
+    RadioButton respuestaUnoPreguntaSiete;
+    @BindView(R.id.respuestaDosPreguntaSiete)
+    RadioButton respuestaDosPreguntaSiete;
+    @BindView(R.id.respuestaTresPreguntaSiete)
+    RadioButton respuestaTresPreguntaSiete;
+    @BindView(R.id.respuestaCuatroPreguntaSiete)
+    RadioButton respuestaCuatroPreguntaSiete;
+    @BindView(R.id.grupoRespuestasSiete)
+    RadioGroup grupoRespuestasSiete;
+    @BindView(R.id.respuestaUnoPreguntaOcho)
+    RadioButton respuestaUnoPreguntaOcho;
+    @BindView(R.id.respuestaDosPreguntaOcho)
+    RadioButton respuestaDosPreguntaOcho;
+    @BindView(R.id.respuestaTresPreguntaOcho)
+    RadioButton respuestaTresPreguntaOcho;
+    @BindView(R.id.respuestaCuatroPreguntaOcho)
+    RadioButton respuestaCuatroPreguntaOcho;
+    @BindView(R.id.grupoRespuestasOcho)
+    RadioGroup grupoRespuestasOcho;
+    @BindView(R.id.respuestaUnoPreguntaNueve)
+    RadioButton respuestaUnoPreguntaNueve;
+    @BindView(R.id.respuestaDosPreguntaNueve)
+    RadioButton respuestaDosPreguntaNueve;
+    @BindView(R.id.respuestaTresPreguntaNueve)
+    RadioButton respuestaTresPreguntaNueve;
+    @BindView(R.id.respuestaCuatroPreguntaNueve)
+    RadioButton respuestaCuatroPreguntaNueve;
+    @BindView(R.id.grupoRespuestasNueve)
+    RadioGroup grupoRespuestasNueve;
+    @BindView(R.id.respuestaUnoPreguntaDiez)
+    RadioButton respuestaUnoPreguntaDiez;
+    @BindView(R.id.respuestaDosPreguntaDiez)
+    RadioButton respuestaDosPreguntaDiez;
+    @BindView(R.id.respuestaTresPreguntaDiez)
+    RadioButton respuestaTresPreguntaDiez;
+    @BindView(R.id.respuestaCuatroPreguntaDiez)
+    RadioButton respuestaCuatroPreguntaDiez;
+    @BindView(R.id.grupoRespuestasDiez)
+    RadioGroup grupoRespuestasDiez;
+    @BindView(R.id.buttonCalificarConversiones)
+    Button buttonCalificarConversiones;
+
+    //el arreglo es creado para poder retener el estado de los checkBox cuando el usuario gira el
+    // dispositivo*/
+    private int[] radioChecked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_examen_conversiones);
+        ButterKnife.bind(this);
+
+        //iniciamos el metodo verificar para saber si es que el layout se a refrescado o es su
+        /*primera vez que se inicia*/
+        verificarEstadoAnterior(savedInstanceState);
+
+        /*si es la primera vez que el layout es inflado se guardara el estado de los checkbox*/
+        if (savedInstanceState == null) {
+            radioChecked = new int[]{
+                    grupoRespuestasUno.getCheckedRadioButtonId(),
+                    grupoRespuestasDos.getCheckedRadioButtonId(),
+                    grupoRespuestasTres.getCheckedRadioButtonId(),
+                    grupoRespuestasCuatro.getCheckedRadioButtonId(),
+                    grupoRespuestasCinco.getCheckedRadioButtonId(),
+                    grupoRespuestasSeis.getCheckedRadioButtonId(),
+                    grupoRespuestasSiete.getCheckedRadioButtonId(),
+                    grupoRespuestasOcho.getCheckedRadioButtonId(),
+                    grupoRespuestasNueve.getCheckedRadioButtonId(),
+                    grupoRespuestasDiez.getCheckedRadioButtonId()};
+        }
+    }
+    //este metodo guarda el estado de los checkbox
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntArray("estado", radioChecked);
+    }
+    //optenemos el estado anterior de la pantalla en caso de que exista uno.
+    private void verificarEstadoAnterior(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            radioChecked = savedInstanceState.getIntArray("estado");
+            setCheckBoxChecked();
+        }
+    }
+    //se asigna el estado anterior de los checkbox
+    private void setCheckBoxChecked() {
+        grupoRespuestasUno.check(radioChecked[0]);
+        grupoRespuestasDos.check(radioChecked[1]);
+        grupoRespuestasTres.check(radioChecked[2]);
+        grupoRespuestasCuatro.check(radioChecked[3]);
+        grupoRespuestasCinco.check(radioChecked[4]);
+        grupoRespuestasSeis.check(radioChecked[5]);
+        grupoRespuestasSiete.check(radioChecked[6]);
+        grupoRespuestasOcho.check(radioChecked[7]);
+        grupoRespuestasNueve.check(radioChecked[8]);
+        grupoRespuestasDiez.check(radioChecked[9]);
     }
 }
--- app/src/main/java/com/example/cesar/aplicacionfisica/ExamenTeoricoVectoresActivity.java
+++ app/src/main/java/com/example/cesar/aplicacionfisica/ExamenTeoricoVectoresActivity.java
@@ -1,13 +1,163 @@
 package com.example.cesar.aplicacionfisica;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ExamenTeoricoVectoresActivity extends AppCompatActivity {
 
+    //se crean las vistas y enlazan con ayuda de butterKnife
+    @BindView(R.id.respuestaUnoPreguntaUnoVectores)
+    RadioButton respuestaUnoPreguntaUnoVectores;
+    @BindView(R.id.respuestaDosPreguntaUnoVectores)
+    RadioButton respuestaDosPreguntaUnoVectores;
+    @BindView(R.id.respuestaTresPreguntaUnoVectores)
+    RadioButton respuestaTresPreguntaUnoVectores;
+    @BindView(R.id.grupoRespuestasUnoVectores)
+    RadioGroup grupoRespuestasUnoVectores;
+    @BindView(R.id.respuestaUnoPreguntaDos)
+    RadioButton respuestaUnoPreguntaDos;
+    @BindView(R.id.respuestaDosPreguntaDosVectores)
+    RadioButton respuestaDosPreguntaDosVectores;
+    @BindView(R.id.respuestaTresPreguntaDosVectores)
+    RadioButton respuestaTresPreguntaDosVectores;
+    @BindView(R.id.grupoRespuestasDosVectores)
+    RadioGroup grupoRespuestasDosVectores;
+    @BindView(R.id.respuestaUnoPreguntaTresVectores)
+    RadioButton respuestaUnoPreguntaTresVectores;
+    @BindView(R.id.respuestaDosPreguntaTresVectores)
+    RadioButton respuestaDosPreguntaTresVectores;
+    @BindView(R.id.respuestaTresPreguntaTresVectores)
+    RadioButton respuestaTresPreguntaTresVectores;
+    @BindView(R.id.grupoRespuestasTresVectores)
+    RadioGroup grupoRespuestasTresVectores;
+    @BindView(R.id.respuestaUnoPreguntaCuatroVectores)
+    RadioButton respuestaUnoPreguntaCuatroVectores;
+    @BindView(R.id.respuestaDosPreguntaCuatroVectores)
+    RadioButton respuestaDosPreguntaCuatroVectores;
+    @BindView(R.id.respuestaTresPreguntaCuatroVectores)
+    RadioButton respuestaTresPreguntaCuatroVectores;
+    @BindView(R.id.grupoRespuestasCuatroVectores)
+    RadioGroup grupoRespuestasCuatroVectores;
+    @BindView(R.id.respuestaUnoPreguntaCincoVectores)
+    RadioButton respuestaUnoPreguntaCincoVectores;
+    @BindView(R.id.respuestaDosPreguntaCincoVectores)
+    RadioButton respuestaDosPreguntaCincoVectores;
+    @BindView(R.id.respuestaTresPreguntaCincoVectores)
+    RadioButton respuestaTresPreguntaCincoVectores;
+    @BindView(R.id.grupoRespuestasCincoVectores)
+    RadioGroup grupoRespuestasCincoVectores;
+    @BindView(R.id.respuestaUnoPreguntaSeisVectores)
+    RadioButton respuestaUnoPreguntaSeisVectores;
+    @BindView(R.id.respuestaDosPreguntaSeisVectores)
+    RadioButton respuestaDosPreguntaSeisVectores;
+    @BindView(R.id.respuestaTresPreguntaSeisVectores)
+    RadioButton respuestaTresPreguntaSeisVectores;
+    @BindView(R.id.respuestaCuatroPreguntaSeisVectores)
+    RadioButton respuestaCuatroPreguntaSeisVectores;
+    @BindView(R.id.grupoRespuestasSeisVectores)
+    RadioGroup grupoRespuestasSeisVectores;
+    @BindView(R.id.respuestaUnoPreguntaSieteVectores)
+    RadioButton respuestaUnoPreguntaSieteVectores;
+    @BindView(R.id.respuestaDosPreguntaSieteVectores)
+    RadioButton respuestaDosPreguntaSieteVectores;
+    @BindView(R.id.respuestaTresPreguntaSieteVectores)
+    RadioButton respuestaTresPreguntaSieteVectores;
+    @BindView(R.id.respuestaCuatroPreguntaSieteVectores)
+    RadioButton respuestaCuatroPreguntaSieteVectores;
+    @BindView(R.id.grupoRespuestasSieteVectores)
+    RadioGroup grupoRespuestasSieteVectores;
+    @BindView(R.id.respuestaUnoPreguntaOchoVectores)
+    RadioButton respuestaUnoPreguntaOchoVectores;
+    @BindView(R.id.respuestaDosPreguntaOchoVectores)
+    RadioButton respuestaDosPreguntaOchoVectores;
+    @BindView(R.id.respuestaTresPreguntaOchoVectores)
+    RadioButton respuestaTresPreguntaOchoVectores;
+    @BindView(R.id.respuestaCuatroPreguntaOchoVectores)
+    RadioButton respuestaCuatroPreguntaOchoVectores;
+    @BindView(R.id.grupoRespuestasOchoVectores)
+    RadioGroup grupoRespuestasOchoVectores;
+    @BindView(R.id.respuestaUnoPreguntaNueveVectores)
+    RadioButton respuestaUnoPreguntaNueveVectores;
+    @BindView(R.id.respuestaDosPreguntaNueveVectores)
+    RadioButton respuestaDosPreguntaNueveVectores;
+    @BindView(R.id.respuestaTresPreguntaNueveVectores)
+    RadioButton respuestaTresPreguntaNueveVectores;
+    @BindView(R.id.respuestaCuatroPreguntaNueveVectores)
+    RadioButton respuestaCuatroPreguntaNueveVectores;
+    @BindView(R.id.grupoRespuestasNueveVectores)
+    RadioGroup grupoRespuestasNueveVectores;
+    @BindView(R.id.respuestaUnoPreguntaDiezVectores)
+    RadioButton respuestaUnoPreguntaDiezVectores;
+    @BindView(R.id.respuestaDosPreguntaDiezVectores)
+    RadioButton respuestaDosPreguntaDiezVectores;
+    @BindView(R.id.respuestaTresPreguntaDiezVectores)
+    RadioButton respuestaTresPreguntaDiezVectores;
+    @BindView(R.id.respuestaCuatroPreguntaDiezVectores)
+    RadioButton respuestaCuatroPreguntaDiezVectores;
+    @BindView(R.id.grupoRespuestasDiezVectores)
+    RadioGroup grupoRespuestasDiezVectores;
+    @BindView(R.id.buttonCalificarVectores)
+    Button buttonCalificarVectores;
+
+    //el arreglo es creado para poder retener el estado de los checkBox cuando el usuario gira el
+    // dispositivo*/
+    private int[] radioChecked;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_examen_teorico_vectores);
+        ButterKnife.bind(this);
+        //iniciamos el metodo verificar para saber si es que el layout se a refrescado o es su
+        /*primera vez que se inicia*/
+        verificarEstadoAnterior(savedInstanceState);
+        /*si es la primera vez que el layout es inflado se guardara el estado de los checkbox*/
+        if(savedInstanceState==null){
+            radioChecked = new int[]{
+                    grupoRespuestasUnoVectores.getCheckedRadioButtonId(),
+                    grupoRespuestasDosVectores.getCheckedRadioButtonId(),
+                    grupoRespuestasTresVectores.getCheckedRadioButtonId(),
+                    grupoRespuestasCuatroVectores.getCheckedRadioButtonId(),
+                    grupoRespuestasCincoVectores.getCheckedRadioButtonId(),
+                    grupoRespuestasSeisVectores.getCheckedRadioButtonId(),
+                    grupoRespuestasSieteVectores.getCheckedRadioButtonId(),
+                    grupoRespuestasOchoVectores.getCheckedRadioButtonId(),
+                    grupoRespuestasNueveVectores.getCheckedRadioButtonId(),
+                    grupoRespuestasDiezVectores.getCheckedRadioButtonId()};
+        }
+
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntArray("estadoAnterior",radioChecked);
+    }
+    //optenemos el estado anterior de la pantalla en caso de que exista uno.
+    private void verificarEstadoAnterior(Bundle savedInstanceState) {
+        if (savedInstanceState!=null){
+            radioChecked = savedInstanceState.getIntArray("estadoAnterior");
+            setCheckBoxChecked();
+        }
+    }
+    //se asigna el estado anterior de los checkbox
+    private void setCheckBoxChecked() {
+        grupoRespuestasUnoVectores.check(radioChecked[0]);
+        grupoRespuestasDosVectores.check(radioChecked[1]);
+        grupoRespuestasTresVectores.check(radioChecked[2]);
+        grupoRespuestasCuatroVectores.check(radioChecked[3]);
+        grupoRespuestasCincoVectores.check(radioChecked[4]);
+        grupoRespuestasSeisVectores.check(radioChecked[5]);
+        grupoRespuestasSieteVectores.check(radioChecked[6]);
+        grupoRespuestasOchoVectores.check(radioChecked[7]);
+        grupoRespuestasNueveVectores.check(radioChecked[8]);
+        grupoRespuestasDiezVectores.check(radioChecked[9]);
+    }
+
 }
